8ba96e0b0be7f40daea7231488968209bfa45dac
toxicmog <toxicmog>
2018-12-02 09:31:19 +0000

    renaming

---
+++ ComicBurst/Controllers/VoidController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicVoid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoidController: ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "hello", "goodbye" };
+        }
+    }
+}
